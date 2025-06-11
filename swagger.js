0f32fb54feb85149bd97deb8faf7e431e74d35c4
Elonnativesystem <ponnumani@elonnativesystem.com>
2025-06-11 11:19:09 +0530

    first commit

---
+++ swagger.js
@@ -0,0 +1,17 @@
+const swaggerAutogen = require("swagger-autogen")();
+
+const doc = {
+    info: {
+        title: "Hike Healthgs private Limited",
+        description: "Version 2.0"
+    },
+    host: "localhost:8000",
+    basePath: "/",
+    schemes: ["http"],
+};
+
+const outputFile = "./swagger-output.json";
+const routes = ["./index.js"];
+
+swaggerAutogen(outputFile, routes, doc);
+
